ef7160351bcdc4b40e13d935c4800b8894426aa5
m31271n <m31271n@gmail.com>
2018-05-26 22:05:02 +0800

    feat: make it robust

--- src/index.js
+++ src/index.js
@@ -6,33 +6,25 @@ function fetchURL(name, url) {
     const xhr = new window.XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'blob';
-
-    function onResolved() {
-      const blob = this.response;
+    function onResolved(ctx) {
+      const blob = ctx.response;
       resolve([name, blob]);
     }
 
     function onRejected() {
-      reject([
-        name,
-        url,
-        {
-          status: this.status,
-          statusText: this.statusText,
-        },
-      ]);
+      reject([name, url]);
     }
 
     xhr.onload = function() {
       if ((this.status >= 200 && this.status < 300) || this.status === 304) {
-        onResolved();
+        onResolved(this);
       } else {
-        onRejected();
+        onRejected(this);
       }
     };
 
     xhr.onerror = function() {
-      onRejected();
+      onRejected(this);
     };
 
     xhr.send();
@@ -41,11 +33,12 @@ function fetchURL(name, url) {
 
 class WebAssetsManager {
   constructor() {
+    this.assets = {};
     this.cache = {};
   }
 
   setup(assets) {
-    this.assets = assets;
+    this.assets = Object.assign(this.assets, assets);
   }
 
   preload(progress, complete) {
@@ -54,10 +47,15 @@ class WebAssetsManager {
     let currentCount = 0;
     let currentProgress = 0;
 
-    const saveAsBlobURL = ([name, blob]) => {
-      const blobURL = URL.createObjectURL(blob);
-      this.cache[name] = blobURL;
-      return [name, blob];
+    const saveAsBlobURL = ([name, data]) => {
+      if (data instanceof Blob) {
+        const blobURL = URL.createObjectURL(data);
+        this.cache[name] = blobURL;
+      } else {
+        this.cache[name] = data;
+      }
+
+      return [name, data];
     };
 
     const handleProgress = () => {
@@ -83,7 +81,15 @@ class WebAssetsManager {
   }
 
   get(name) {
-    return this.cache[name] || this.assets[name];
+    if (this.cache[name]) {
+      return this.cache[name];
+    } else {
+      // eslint-disable-next-line
+      console.warn(
+        "[web-assets-manager] You are get assets via URL, not from cache. Although it works, but it's better to check it now."
+      );
+      return this.assets[name];
+    }
   }
 }
 
